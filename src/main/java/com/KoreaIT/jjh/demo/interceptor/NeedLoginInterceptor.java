69c8b583f8431ba488dfe88c7828fe79a516ca98
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-04-21 15:23:52 +0900

    NeedLoginInterceptor가 각 액션이 실행되기 전에 로그인 체크를 앞단에서 미리 수행함

--- src/main/java/com/KoreaIT/jjh/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/KoreaIT/jjh/demo/interceptor/NeedLoginInterceptor.java
@@ -6,13 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.KoreaIT.jjh.demo.vo.ResultData;
+import com.KoreaIT.jjh.demo.vo.Rq;
+
 @Component
 public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		Rq rq = (Rq) req.getAttribute("rq");
 
-		System.out.println("로그인 하고 써라");
+		if (!rq.isLogined()) {
+//			resp.getWriter().append("<script~~~~)
+
+			rq.printHitoryBackJs("로그인 후 이용해 주세요");
+			return false;
+		}
 
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
