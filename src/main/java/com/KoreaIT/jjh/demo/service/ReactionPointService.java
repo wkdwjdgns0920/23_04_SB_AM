65e5c8572f08b1bd2a238d433c9e64f9b26c8b69
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-04-28 15:58:44 +0900

    버튼 누를 때의 좋아요, 싫어요 구현

--- src/main/java/com/KoreaIT/jjh/demo/service/ReactionPointService.java
+++ src/main/java/com/KoreaIT/jjh/demo/service/ReactionPointService.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.KoreaIT.jjh.demo.repository.ReactionPointRepository;
+import com.KoreaIT.jjh.demo.vo.ResultData;
 
 @Service
 public class ReactionPointService {
 
 	@Autowired
 	private ReactionPointRepository reactionPointRepository;
+	@Autowired
+	private ArticleService articleService;
 
 	public boolean actorCanMakeReaction(int actorId, String relTypeCode, int relId) {
 		if (actorId == 0) {
@@ -18,4 +21,33 @@ public class ReactionPointService {
 		return reactionPointRepository.getSumReactionPointByMemberId(actorId, relTypeCode, relId) == 0;
 	}
 
-}
+	public ResultData addGoodReactionPoint(int actorId, String relTypeCode, int relId) {
+		int affectedRow = reactionPointRepository.addGoodReactionPoint(actorId, relTypeCode, relId);
+
+		if (affectedRow != 1) {
+			return ResultData.from("F-2", "좋아요 실패");
+		}
+
+		switch (relTypeCode) {
+		case "article":
+			articleService.increaseGoodReationPoint(relId);
+			break;
+		}
+
+		return ResultData.from("S-1", "좋아요 처리 됨");
+
+	}
+
+	public ResultData addBadReactionPoint(int actorId, String relTypeCode, int relId) {
+		reactionPointRepository.addBadReactionPoint(actorId, relTypeCode, relId);
+
+		switch (relTypeCode) {
+		case "article":
+			articleService.increaseBadReationPoint(relId);
+			break;
+		}
+
+		return ResultData.from("S-1", "싫어요 처리 됨");
+	}
+
+}	
