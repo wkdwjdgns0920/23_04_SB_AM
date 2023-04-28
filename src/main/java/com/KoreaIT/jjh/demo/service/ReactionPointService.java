9c9a3d6462849a78f201a3106c4c2e33de36f863
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-04-28 14:31:02 +0900

    reactionPointService, reactionPointRepository 도입

---
+++ src/main/java/com/KoreaIT/jjh/demo/service/ReactionPointService.java
@@ -0,0 +1,21 @@
+package com.KoreaIT.jjh.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.KoreaIT.jjh.demo.repository.ReactionPointRepository;
+
+@Service
+public class ReactionPointService {
+
+	@Autowired
+	private ReactionPointRepository reactionPointRepository;
+
+	public boolean actorCanMakeReaction(int actorId, String relTypeCode, int relId) {
+		if (actorId == 0) {
+			return false;
+		}
+		return reactionPointRepository.getSumReactionPointByMemberId(actorId, relTypeCode, relId) == 0;
+	}
+
+}
