fb7db936fe34a16917eff72d2236ed4f5a8e4a23
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-05-02 16:26:49 +0900

    댓글 폼 체크

---
+++ src/main/java/com/KoreaIT/jjh/demo/vo/Reply.java
@@ -0,0 +1,19 @@
+package com.KoreaIT.jjh.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reply {
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private int memberId;
+	private String relTypeCode;
+	private int relId;
+	private String body;
+
+}
