5fcfc7e5c9117e66046a4ddc2c9ba6ad11725205
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-05-04 14:08:48 +0900

    처음부터 무조건 rq 객체를 만들지 않고 필요할 때만 생성하도록

--- src/main/java/com/KoreaIT/jjh/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/jjh/demo/controller/UsrHomeController.java
@@ -2,13 +2,20 @@ package com.KoreaIT.jjh.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.KoreaIT.jjh.demo.vo.Rq;
 
 @Controller
 public class UsrHomeController {
+	private Rq rq;
+
+	public UsrHomeController(Rq rq) {
+		this.rq = rq;
+	}
 
 	@RequestMapping("/usr/home/main")
 	public String showMain() {
+		rq.run();
 		return "usr/home/main";
 	}
 
