d8f7fd81514783477e0524f8c44305f7094dc889
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-05-11 15:39:56 +0900

    관리자 페이지 추가, 관리자만 접근할 수 있도록, 모든 회원과 모든 게시글을 컨트롤 할 수 있도록 구현 예정

--- src/main/java/com/KoreaIT/jjh/demo/controller/UsrMemberController.java
+++ src/main/java/com/KoreaIT/jjh/demo/controller/UsrMemberController.java
@@ -1,7 +1,5 @@
 package com.KoreaIT.jjh.demo.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/KoreaIT/jjh/demo/repository/MemberRepository.java
+++ src/main/java/com/KoreaIT/jjh/demo/repository/MemberRepository.java
@@ -77,14 +77,74 @@ public interface MemberRepository {
 			</script>
 			""")
 	void modify(int id, String loginPw, String name, String nickname, String cellphoneNum, String email);
-	
-	
+
 	@Select("""
-			SELECT *
-			FROM `member`
-			WHERE `name` = #{name}
-			AND email = #{email}
-			""")
-	Member getMemberByName(String name,String email);
+			<script>
+			SELECT COUNT(*) AS cnt
+			FROM `member` AS M
+			WHERE 1
+			<if test="authLevel != 0">
+				AND M.authLevel = #{authLevel}
+			</if>
+			<if test="searchKeyword != ''">
+				<choose>
+					<when test="searchKeywordTypeCode == 'loginId'">
+						AND M.loginId LIKE CONCAT('%', #{searchKeyword}, '%')
+					</when>
+					<when test="searchKeywordTypeCode == 'name'">
+						AND M.name LIKE CONCAT('%', #{searchKeyword}, '%')
+					</when>
+					<when test="searchKeywordTypeCode == 'nickname'">
+						AND M.nickname LIKE CONCAT('%', #{searchKeyword}, '%')
+					</when>
+					<otherwise>
+						AND (
+							M.loginId LIKE CONCAT('%', #{searchKeyword}, '%')
+							OR M.name LIKE CONCAT('%', #{searchKeyword}, '%')
+							OR M.nickname LIKE CONCAT('%', #{searchKeyword}, '%')
+							)
+					</otherwise>
+				</choose>
+			</if>
+			</script>
+							""")
+	int getMembersCount(String authLevel, String searchKeywordTypeCode, String searchKeyword);
+
+	@Select("""
+			<script>
+			SELECT M.*
+			FROM `member` AS M
+			WHERE 1
+			<if test="authLevel != 0">
+				AND M.authLevel = #{authLevel}
+			</if>
+			<if test="searchKeyword != ''">
+				<choose>
+					<when test="searchKeywordTypeCode == 'loginId'">
+						AND M.loginId LIKE CONCAT('%', #{searchKeyword}, '%')
+					</when>
+					<when test="searchKeywordTypeCode == 'name'">
+						AND M.name LIKE CONCAT('%', #{searchKeyword}, '%')
+					</when>
+					<when test="searchKeywordTypeCode == 'nickname'">
+						AND M.nickname LIKE CONCAT('%', #{searchKeyword}, '%')
+					</when>
+					<otherwise>
+						AND (
+							M.loginId LIKE CONCAT('%', #{searchKeyword}, '%')
+							OR M.name LIKE CONCAT('%', #{searchKeyword}, '%')
+							OR M.nickname LIKE CONCAT('%', #{searchKeyword}, '%')
+							)
+					</otherwise>
+				</choose>
+			</if>
+			ORDER BY M.id DESC
+				<if test="limitTake != -1">
+					LIMIT #{limitStart}, #{limitTake}
+				</if>
+			</script>
+							""")
+	List<Member> getForPrintMembers(String authLevel, String searchKeywordTypeCode, String searchKeyword,
+			int limitStart, int limitTake);
 
 }
